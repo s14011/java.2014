6c0c229922d10261daf307d7642a7489ffd15cd4
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-09-26 22:17:18 +0900

    ex2のひな形作成

---
+++ Ex2.java
@@ -0,0 +1,21 @@
+class Ex2_0 {
+	void execute(){
+		int x;
+		int y;
+
+		x = 63;
+		y = 18;
+
+		System.out.println("xの値は" + x + "です。");
+		System.out.println("yの値は" + y + "です。");
+		System.out.println("合計は" + (x + y) + "です。");
+		System.out.println("平均は" + (x + y) / 2 + "です。");
+	}
+}
+
+public class Ex2 {
+	public static void main(String[] args){
+		Ex2_0 a = new Ex2_0();
+		a.execute();
+	}
+}
