97b730d8dcf02f73111e8797ac38661bd5d23489
Kimiyuki Yamauchi <yamauchi@std.it-college.ac.jp>
2014-09-26 12:00:59 +0900

    ex1のひな形を追加

---
+++ Ex1.java
@@ -0,0 +1,12 @@
+class Ex1_0 {
+	void execute(){
+		System.out.println("Hello World!");
+	}
+}
+
+public class Ex1 {
+	public static void main(String[] args){
+		Ex1_0 a = new Ex1_0();
+		a.execute();
+	}
+}
