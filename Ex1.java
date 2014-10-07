70044683f195300e1f05cc54f13404f172983b50
s14011 <s14011@std.it-college.ac.jp>
2014-10-07 12:15:35 +0900

    演習1-3作成

--- Ex1.java
+++ Ex1.java
@@ -10,16 +10,22 @@ class Ex1_0 extends Ex {
 
 class Ex1_2 extends Ex {
 	void execute(){
-		System.out.println("山\n内\n公\n之");
+		System.out.println("山\n内\n亮\n和");
 	}
 }
-
+class Ex1_3 extends Ex {
+	void execute(){
+		System.out.println("山\n内\n\n亮\n和");
+	}
+ }
 public class Ex1 {
 	public static void main(String[] args){
 		Ex a;
 		a = new Ex1_0();
 		a.execute();
 		a = new Ex1_2();
+		a.execute();
+		a = new Ex1_3();
 		a.execute();
 	}
 }
