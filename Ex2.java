f629d854dffb7e7947153fe9193436b96a391488
s14011 <s14011@std.it-college.ac.jp>
2014-10-15 11:02:34 +0900

    chap1演習　10/14

--- Ex2.java
+++ Ex2.java
@@ -20,10 +20,49 @@ class Ex2_0 extends Ex{
 	}
 }
 
+class Ex2_1 extends Ex{
+	void execute(){
+		double x;
+		double y;
+
+		x = 63;
+		y = 18;
+
+		System.out.println("xの値は" + x + "です。");
+		System.out.println("yの値は" + y + "です。");
+		System.out.println("合計は" + (x + y) + "です。");
+		System.out.println("平均は" + (x + y) / 2 + "です。");
+	}
+}
+
+class Ex2_2 extends Ex{
+	void execute(){
+		int x;
+		int y;
+		int z;
+
+		x = 63;
+		y = 18;
+		z = 30;   
+
+		System.out.println("xの値は" + x + "です。");
+		System.out.println("yの値は" + y + "です。");
+		System.out.println("zの値は" + z + "です。");
+		System.out.println("合計は" + (x + y + z) + "です。");
+		System.out.println("平均は" + (x + y + z) / 2 + "です。");
+	}
+}
+
+
+
 public class Ex2 {
 	public static void main(String[] args){
 		Ex a;
 		a = new Ex2_0();
+		a.execute();
+		a = new Ex2_1();
+		a.execute();
+		a = new Ex2_2();
 		a.execute();
 	}
 }
