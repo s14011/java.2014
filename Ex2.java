c94fe5fbaa65e724715f39c9f20f110ee20afcc5
Ryowa Yamauchi <s14011@std.it-college.ac.jp>
2014-10-21 12:09:50 +0900

    10/21 演習

--- Ex2.java
+++ Ex2.java
@@ -53,6 +53,64 @@ class Ex2_2 extends Ex{
 	}
 }
 
+class Ex2_3 extends Ex{
+	void execute(){
+	Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("整数値:");
+		int a = stdIn.nextInt();
+
+		System.out.println( a+ "です。");
+			 
+ }
+}
+
+
+class Ex2_4 extends Ex{
+	void execute(){
+	Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("整数値:");
+		int a = stdIn.nextInt();
+
+		System.out.println("10を加えた値は" +(a+10)+ "です。");
+		System.out.println("10を減じた値は" +(a-10)+ "です。");	
+ 
+ }
+}
+
+class Ex2_5 extends Ex{
+	void execute(){
+	Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("xの値:");
+		int x = stdIn.nextInt();
+
+		System.out.print("yの値:");
+		int y = stdIn.nextInt();
+
+
+		System.out.println((x + y) +   "です。");
+		System.out.println((x + y)/2 + "です。");	
+ 
+ }
+}
+
+class Ex2_6 extends Ex{
+	void execute(){
+	Scanner stdIn = new Scanner(System.in);
+
+		System.out.print("底辺:");
+		double x = stdIn.nextDouble();
+
+		System.out.print("高さ:");
+		double y = stdIn.nextDouble();
+
+
+		System.out.println(( x * y) /2 +  "です。");	
+ 
+ }
+}
 
 
 public class Ex2 {
@@ -63,6 +121,14 @@ public class Ex2 {
 		a = new Ex2_1();
 		a.execute();
 		a = new Ex2_2();
+		a.execute();
+		a = new Ex2_3();
+		a.execute();
+		a = new Ex2_4();
+		a.execute();
+		a = new Ex2_5();
+		a.execute();
+		a = new Ex2_6();
 		a.execute();
 	}
 }
