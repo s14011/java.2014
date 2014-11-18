2f11f6c151fde696fbb977b2446fefc0a4e8f25f
Ryowa Yamauchi <s14011@std.it-college.ac.jp>
2014-11-18 09:33:27 +0900

    merge

--- Ex3.java
+++ Ex3.java
@@ -12,33 +12,41 @@ class Ex3{
 	}
 
 	public int ex3_1(int n){
-	Scanner scn = new Scanner(System.in);
+		
+	return Math.abs(n);
 
-	Syatem.out.print("整数値:");
-	int n = scn.nextInt();
-
-	if (n > 0) {	
-//		return -1;
-	n = n * 1;
-	}
-	System.out.print(n);
 	}
 
 	public String ex3_2(int a, int b){
-
-		return "dummy";
+	
+	
+	if (a % b ==0){
+		return b + "は" + a + "の約数です";
+	}else{
+		return "5は12の約数ではありません";
+	}
 	}
 
 	public String ex3_3(int a, int b){
-
-		return "dummy";
-
+	
+	if(a > b){
+		return "8の方が大きいです";
+	}else if(a == b){
+		return "同じ値です";
+	}else{
+		return "9の方が大きいです";
+	}
 	}
 
 	public String ex3_4(int a){
-
-		return "dummy";
-
+	
+	if (a % 5 ==0){
+		return "その値は5で割り切れます";
+	}else if(a %  5 > 0){
+		return "その値は5で割り切れません";
+	}else{
+		return "正でない整数値です";
+	}
 	}
 
 	public String ex3_5(int a){
